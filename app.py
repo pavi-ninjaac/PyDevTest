acb98ca6bdaa2105244467e31539c4193fa00891
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2021-04-25 10:08:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask , jsonify , Response
+import json
+from core import app
+
+# import all the routers from the routes file in core directory
+from core import routes
+
+#handle the 404 error here
+@app.errorhandler(404)
+def pageNotFound(error):
+    return Response(
+                    response = 
+                    json.dumps({"message":"No such route(page) is available........."},default=str), 
+                    status=404,
+                    mimetype="application/json"
+                    )
+
+#run the flask app
+if __name__ == "__main__":
+    app.run(debug=True , port=3000)
